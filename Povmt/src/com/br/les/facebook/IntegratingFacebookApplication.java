5e223d0d2bed8f0ffcfa258a0b029848cf49cbd4
Juan Almeida <juan.satyro@gmail.com>
2015-04-27 20:53:55 -0300

    add

---
+++ Povmt/src/com/br/les/facebook/IntegratingFacebookApplication.java
@@ -0,0 +1,20 @@
+package com.br.les.facebook;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class IntegratingFacebookApplication extends Application {
+
+  public static final String TAG = "MyApp";
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    Parse.initialize(this, 
+        "gK6txX8ALKEZKX4fSU3GvYGQCBhYKJ0fbouIqVXN",
+        "agdnZxR0G9Z8xOhfYYOmFmsd9HKN0lwz7MpEwntH"
+    );
+  }
+}
